b3056144b2491d4b2b02b926518f928bda00bb63
Oğuzhan Eroğlu <rohanrhu2@gmail.com>
2024-04-29 23:05:17 +0300

    Remove SSR for play page

--- src/routes/play/+page.server.ts
+++
@@ -1,7 +0,0 @@
-export function load({setHeaders}) {
-    setHeaders({
-        'Access-Control-Allow-Origin': '*',
-        'Cross-Origin-Opener-Policy': 'unsafe-none',
-        'Cross-Origin-Embedder-Policy': 'credentialless'
-    });
-}
